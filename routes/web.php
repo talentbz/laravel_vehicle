a3310909fc8cf4a3c51388524cca2e6994eee601
talentbz <82767636+talentbz@users.noreply.github.com>
2021-08-31 11:00:19 +0900

    fornt test

--- routes/web.php
+++ routes/web.php
@@ -65,5 +65,5 @@ Route::post('/update-password/{id}', [App\Http\Controllers\HomeController::class
 
 Route::get('{any}', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 
-//Language Translation1
+//Language Translation test
 Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang']);
